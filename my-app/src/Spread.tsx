ec0c3e53a3a3b6d76494a5f3d42365d8de03db74
Tetra <tanya.prokhorenko.2017@mail.ru>
2023-04-10 16:44:32 +0300

    fix to problem with enter average total cost crypto

--- my-app/src/Spread.tsx
+++ my-app/src/Spread.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 interface Currency {
   name: string;
@@ -7,71 +6,86 @@ interface Currency {
   quantity: number;
 }
 
-const Spread: React.FC = () => {
-  const [currencies, setCurrencies] = useState<Currency[]>([]);
-  const [initialValue, setInitialValue] = useState(0);
-  const [currentValue, setCurrentValue] = useState(0);
+interface SpreadState {
+  currencies: Currency[];
+  initialValue: number;
+  currentValue: number;
+}
 
-  useEffect(() => {
-    // Получаем данные из локального хранилища и устанавливаем начальную стоимость портфеля
+class Spread extends React.Component<{}, SpreadState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      currencies: [],
+      initialValue: 0,
+      currentValue: 0,
+    };
+  }
+
+  componentDidMount() {
     const storedCurrencies = localStorage.getItem('bagItems');
     if (storedCurrencies) {
       const parsedCurrencies = JSON.parse(storedCurrencies);
-      setCurrencies(parsedCurrencies);
+      this.setState({ currencies: parsedCurrencies });
+// 
       const initialValue = parsedCurrencies.reduce(
         (total: number, currency: Currency) => total + currency.price * currency.quantity,
         0,
       );
-      setInitialValue(initialValue);
+      this.setState({ initialValue });
+      this.fetchCurrencies();
     }
-  }, []);
+  }
 
-  async function fetchCurrencies() {
+  async fetchCurrencies() {
     const response = await fetch('https://api.coincap.io/v2/assets?limit=100');
     const data = await response.json();
-    
-    // Используем параметр функции map для доступа к индексу текущего элемента массива
+    const { currencies } = this.state;
+console.log(currencies)
     const updatedCurrencies = currencies.map((item, index) => {
-      const foundCurrency = data.data.find((currency: any) => currency.symbol === item.name);
- console.log(updatedCurrencies);
+      const foundCurrency = data.data.find((currency: Currency) => currency.name === item.name);
+// console.log(currency )
       if (foundCurrency) {
-       
         return {
           ...item,
           price: foundCurrency.priceUsd,
-          
         };
       }
       return item;
+// 
+
     });
-    setCurrencies(updatedCurrencies);
+    this.setState({ currencies: updatedCurrencies });
   }
 
-  useEffect(() => {
-    const currentValue = currencies.reduce(
-      (total: number, currency: Currency) => total + currency.price * currency.quantity,
-      0,
-    );
-    setCurrentValue(currentValue);
-  }, [currencies, currentValue]);
-  
-  
-//   console.log(currentValue);
+  componentDidUpdate(prevProps: {}, prevState: SpreadState) {
+    const { currencies } = this.state;
+    if (currencies !== prevState.currencies) {
+      const currentValue = currencies.reduce(
+        (total: number, currency: Currency) => total + currency.price * currency.quantity,
+        0,
+      );
+      this.setState({ currentValue });
+    }
+  }
 
-  const difference = initialValue - currentValue;
-  const differencePercent = ((difference / initialValue) * 100).toFixed(2);
+  render() {
+    const { initialValue, currentValue } = this.state;
+    const difference = initialValue - currentValue;
+    const differencePercent = ((difference / initialValue) * 100).toFixed(2);
 
-  return (
-    <div className="header">
-      <h3>You're Crypto</h3>
-      <div className="portfolio">
-        <span className="value">{initialValue.toFixed(2)} USD </span>
-        <span className={`difference ${difference >= 0 ? 'positive' : 'negative'}`}>
-          {currentValue.toFixed(2)} USD ({differencePercent}%)
-        </span>
+    return (
+      <div className="header">
+        <h3>You're Crypto</h3>
+        <div className="portfolio">
+          <span className="value">{initialValue.toFixed(2)} USD </span>
+          <span className={`difference ${difference >= 0 ? 'positive' : 'negative'}`}>
+            {currentValue.toFixed(2)} USD ({differencePercent}%)
+          </span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Spread;
