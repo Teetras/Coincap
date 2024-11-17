3bbb72fb293e13def4509c58a4a11473f43cb945
Tetra <tanya.prokhorenko.2017@Gmail.com>
2024-11-17 18:22:24 +0300

    mb it's fail

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import CryptoTable from "./Table/CryptoTable";
 import Header from "./Header/Header";
 import "./App.module.scss";
--- my-app/src/Header/Header.tsx
+++ my-app/src/Header/Header.tsx
@@ -1,50 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import styles from "./Header.module.scss"
-import Spread from './Spread';
-import BagInfo from '../Modal elements/Bag';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styles from "./Header.module.scss";
+import Spread from "./Spread";
+import BagInfo from "../Modal elements/Bag";
 
-const Header: React.FC=()=>{
-    const [topCrypto, setTopCrypto] = useState([]);
-    const [isBagInfoOpen, setIsBagInfoOpen] = useState(false);
+const Header: React.FC = () => {
+  const [topCrypto, setTopCrypto] = useState([]);
+  const [isBagInfoOpen, setIsBagInfoOpen] = useState(false);
 
-    useEffect(() => {
-        const fetchCryptos = async () => {
-          const response = await axios.get(
-            "https://api.coincap.io/v2/assets?limit=3"
-          )
-          setTopCrypto(response.data.data.map((crypto: any) => ({
-            id: crypto.id,
-            name: crypto.name,
-            symbol: crypto.symbol,
-            price: parseFloat(crypto.priceUsd),
-          })))
-        };
-        fetchCryptos();
-      }, [])
-      const handleBagInfoOpen = () => {
-        setIsBagInfoOpen(true);
-      };
-      const handleCloseBag = () => {
-        setIsBagInfoOpen(false);
-      };
-    
-      return ( 
-      <div className={styles.head}>
-        <div className={styles.topCrypto} >
-           <p> Top Crypta</p>
-          {topCrypto.map((crypto: any) => (
-
-            <p className={styles.listCript} key={crypto.id}>
-              {crypto.name}: ${crypto.price.toFixed(2)}
-            </p>
-          ))} 
-        </div>
-  <Spread/>
-          <button className={styles.btnOpenBag} onClick={handleBagInfoOpen}>Open Bag</button>
-         
-         {isBagInfoOpen && <BagInfo handleCloseBag={handleCloseBag} />}
-       </div>
+  useEffect(() => {
+    const fetchCryptos = async () => {
+      const response = await axios.get(
+        "https://api.coincap.io/v2/assets?limit=3"
       );
+      setTopCrypto(
+        response.data.data.map((crypto: any) => ({
+          id: crypto.id,
+          name: crypto.name,
+          symbol: crypto.symbol,
+          price: parseFloat(crypto.priceUsd),
+        }))
+      );
+    };
+    fetchCryptos();
+  }, []);
+
+  const handleBagInfoOpen = () => {
+    setIsBagInfoOpen(true);
+  };
+  
+  const handleCloseBag = () => {
+    setIsBagInfoOpen(false);
+  };
+
+  return (
+    <div className={styles.head}>
+      <div className={styles.topCrypto}>
+        <p> Top Crypta</p>
+        {topCrypto.map((crypto: any) => (
+          <p className={styles.listCript} key={crypto.id}>
+            {crypto.name}: ${crypto.price.toFixed(2)}
+          </p>
+        ))}
+      </div>
+      <Spread />
+      <button className={styles.btnOpenBag} onClick={handleBagInfoOpen}>
+        Open Bag
+      </button>
+
+      {isBagInfoOpen && <BagInfo handleCloseBag={handleCloseBag} />}
+    </div>
+  );
 };
-export default Header
+export default Header;
